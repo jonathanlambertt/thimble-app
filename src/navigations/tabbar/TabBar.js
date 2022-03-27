e042b4f0d9f0f0c6a07a4b4908c16706be95031b
Jonathan Lambert <jonathanlambert247@gmail.com>
2022-03-26 22:43:28 -0700

    add icomoon icons to tab bar view

--- src/navigations/tabbar/TabBar.js
+++ src/navigations/tabbar/TabBar.js
@@ -1,20 +1,61 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SearchStack from "../search/SearchStack";
 import TestScreen from "../../screens/TestScreen";
+import { useFonts } from "expo-font";
+import { createIconSetFromIcoMoon } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
+const tabIconSize = 29;
+
+const Icon = createIconSetFromIcoMoon(
+  require("../../../assets/icomoon/selection.json"),
+  "IcoMoon",
+  "icomoon.ttf"
+);
 
 const TabBar = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="SearchStack"
-        component={SearchStack}
-        options={{ headerShown: false }}
-      />
-      <Tab.Screen name="Test" component={TestScreen} />
-    </Tab.Navigator>
-  );
+  const [fontLoaded] = useFonts({
+    IcoMoon: require("../../../assets/icomoon/icomoon.ttf"),
+  });
+
+  if (!fontLoaded) {
+    return null;
+  } else {
+    return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#333",
+          tabBarInactiveTintColor: "#a9a9a9",
+          tabBarIcon: ({ focused, color }) => {
+            // Search tab icon
+            if (route.name == "SearchTab") {
+              if (focused) {
+                return (
+                  <Icon name="search2-fill" size={tabIconSize} color={color} />
+                );
+              } else {
+                return (
+                  <Icon
+                    name="search2-outline"
+                    size={tabIconSize}
+                    color={color}
+                  />
+                );
+              }
+            }
+          },
+        })}
+      >
+        <Tab.Screen
+          name="SearchTab"
+          component={SearchStack}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen name="Test" component={TestScreen} />
+      </Tab.Navigator>
+    );
+  }
 };
 
 export default TabBar;
