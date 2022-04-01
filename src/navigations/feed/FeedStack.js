2953044bd3463031321a56dcbdfc9bf54a34b75e
Jonathan Lambert <jonathanlambert247@gmail.com>
2022-04-01 09:42:59 -0700

    add activity screen (not functional)

--- src/navigations/feed/FeedStack.js
+++ src/navigations/feed/FeedStack.js
@@ -1,10 +1,13 @@
+import { TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import FeedScreen from "../../screens/feed/FeedScreen";
+import ActivityScreen from "../../screens/feed/ActivityScreen";
 import Logo from "../../components/atoms/Logo";
+import { Fontisto, AntDesign } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 
-const FeedStack = () => {
+const FeedStack = ({ navigation }) => {
   return (
     <Stack.Navigator>
       <Stack.Screen
@@ -12,6 +15,31 @@ const FeedStack = () => {
         component={FeedScreen}
         options={{
           title: <Logo />,
+          headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Activity")}>
+              <Fontisto
+                name="bell"
+                size={26}
+                color="black"
+                style={{ marginRight: 20 }}
+              />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Activity"
+        component={ActivityScreen}
+        options={{
+          headerBackTitleVisible: false,
+          headerBackImage: () => (
+            <AntDesign
+              name="arrowleft"
+              size={24}
+              color="black"
+              style={{ marginLeft: 17 }}
+            />
+          ),
         }}
       />
     </Stack.Navigator>
---
+++ src/screens/feed/ActivityScreen.js
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+const ActivityScreen = () => {
+  return <View style={{ flex: 1, backgroundColor: "#fafafa" }}></View>;
+};
+
+export default ActivityScreen;
